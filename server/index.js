24c7b19be6278abecde654293c168011810c0027
Mark Mugisha <141035540+markmugisha@users.noreply.github.com>
2023-08-01 16:17:58 +0300

    Data modelling, Authentication and Authorizationin Node

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js"
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -34,6 +35,9 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
+/* ROUTES WITH FILES*/
+app.post("/auth/register", upload.single("picture"), register)
+
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
@@ -43,4 +47,3 @@ mongoose.connect(process.env.MONGO_URL, {
     app.listen(PORT, () => console.log(`Server listening at Port: ${PORT}`))
 }).catch((error) => console.log(`${error} did not connect`))
 
-//This is a comment
